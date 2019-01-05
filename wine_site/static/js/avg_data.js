32728715cf79052e3bab4181694c7672bcc8a3aa
khantapob <khantapob@gmail.com>
2019-01-05 10:47:16 -0700

    fixed plot

--- wine_site/static/js/avg_data.js
+++ wine_site/static/js/avg_data.js
@@ -1,87 +1,45 @@
 var data = [{
-  country: "Switzerland",
-  average_price: 85.28571429
+  country: "Argentina",
+  average_price: 24.71
 },
 {
-  country: "England ",
-  average_price: 51.68115942
+  country: "Australia",
+  average_price: 35.66
 },
 {
-  country: "Germany",
-  average_price: 42.25754717
+  country: "Chile",
+  average_price: 21.10
 
 },
 
 {
   country: "France",
-  average_price: 41.13912016
-
-},
-
-{
-  country: "Hungary",
-  average_price: 40.64827586
+  average_price: 41.51
 
 },
 
 {
   country: "Italy",
-  average_price: 39.66376966
+  average_price: 38.72
+
+},
+
+{
+  country: "South Africa",
+  average_price: 24.68
 
 },
 
 {
   country: "US",
-  average_price: 36.57346356
-
-},
-
-{
-  country: "Canada",
-  average_price: 35.71259843
-
-},
-
-{
-  country: "Australia",
-  average_price: 35.43766347
-
-},
-
-{
-  country: "Isreal",
-  average_price: 31.76891616
-
-},
-
-{
-  country: "Austria",
-  average_price: 30.76277242
-
-},
-
-{
-  country: "Lebanon",
-  average_price: 30.68571429
+  average_price: 36.07
 
 },
 
 {
   country: "Spain",
-  average_price: 28.21527461
+  average_price: 27.68
 
-},
-
-{
-  country: "New Zealand",
-  average_price: 26.9317852
-
-},
-
-{
-  country: "Mexico",
-  average_price: 26.78571429
-
-},
+}
 
 ];
